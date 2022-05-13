8f3e409020f634322f22f60bc13d73d72efeacb8
Allan Goodman <allan.goodman@student.unsw.edu.au>
2022-05-13 11:56:34 +1000

    added remaining subcom member names to top of file

---
+++ illuminate_uncontained/illuminate_uncontained.ino
@@ -0,0 +1,37 @@
+/*
+ * UNSW Illuminate 2022 - [Un]contained - LED Control
+ * Written by:
+ *    - Allan Goodman
+ *    - Mitchell Wallace
+ *    - Iniyan Vigneswaran
+ *    - Leonard Chiang
+ *    - Janice Nyoto
+ *    - Nila Riahi
+ */
+
+//This function returns boolean of if an object has been detected
+int isDetected(){
+  
+}
+
+//This function writes colour to the LEDs 
+//Root determines the type of LED lighting
+void generateLED(int root)
+
+void setup() {
+  //Initialise pins
+
+  //Initialise sensors
+
+  //
+
+}
+
+void loop() {
+  //Check sensors
+  if(isDetected()){
+    
+  }
+  //
+
+}
