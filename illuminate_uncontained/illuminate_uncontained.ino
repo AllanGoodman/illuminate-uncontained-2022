62cfce66eadda7581e06c474413fafea957b02e7
Allan Goodman <allan.goodman@student.unsw.edu.au>
2022-05-16 15:03:40 +1000

    Added setup LED control code

--- illuminate_uncontained/illuminate_uncontained.ino
+++ illuminate_uncontained/illuminate_uncontained.ino
@@ -9,6 +9,32 @@
  *    - Nila Riahi
  */
 
+ #include <FastLED.h>
+
+//Initialise pins numbers
+#define LED_DATA                    5 //Data pin on digital pin 5
+
+#define ULTRASONIC_SENSOR_1_TRIG    0
+#define ULTRASONIC_SENSOR_1_ECHO    0
+#define ULTRASONIC_SENSOR_2_TRIG    0
+#define ULTRASONIC_SENSOR_2_ECHO    0
+#define ULTRASONIC_SENSOR_3_TRIG    0
+#define ULTRASONIC_SENSOR_3_ECHO    0
+
+//Misc LED initialisation stuff
+#define COLOUR_ORDER       GRB
+#define LED_TYPE           WS2811
+#define BRIGHTNESS         64
+#define NUM_LEDS           120 //120 LEDs on the strip we'll be using
+#define UPDATES_PER_SECOND 100
+CRGB leds[NUM_LEDS];
+CRGBPalette16              currentPalette; //sets what colours we can use
+/*
+ * can also use the following preset palettes for other colours:
+ * RainbowColors_p, RainbowStripeColors_p, OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+ */
+TBlendType                 currentBlending;
+
 //This function returns boolean of if an object has been detected
 int isDetected(){
   
@@ -16,15 +42,22 @@ int isDetected(){
 
 //This function writes colour to the LEDs 
 //Root determines the type of LED lighting
-void generateLED(int root)
+void generateLED(int root){
+  FastLED.show();
+  FastLED.delay(1000/UPDATES_PER_SECOND);
+}
 
 void setup() {
+  delay(3000); //power-up safely delay
   //Initialise pins
 
   //Initialise sensors
 
-  //
-
+  //Initialise LEDs
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  currentPalette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
 }
 
 void loop() {
@@ -32,6 +65,4 @@ void loop() {
   if(isDetected()){
     
   }
-  //
-
 }
