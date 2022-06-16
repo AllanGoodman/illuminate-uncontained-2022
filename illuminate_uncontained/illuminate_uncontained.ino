848f7fdf84f2e0c8c59aeab9685c75eac7e6e6c4
JaniceNyoto <janicefelicia12@gmail.com>
2022-06-16 16:40:29 +1000

    Updated ribbon code

--- illuminate_uncontained/illuminate_uncontained.ino
+++ illuminate_uncontained/illuminate_uncontained.ino
@@ -12,36 +12,31 @@
  #include <FastLED.h>
 
 //Initialise pins numbers
-#define LED_DATA_HAND               5 //Data pin on digital pin 5
+#define LED_DATA_HAND               0 //Data pin on digital pin 5
 #define LED_DATA_RIBBON_0           3
-#define LED_DATA_RIBBON_1           6
-#define LED_DATA_RIBBON_2           4
+#define LED_DATA_RIBBON_1           4
+#define LED_DATA_RIBBON_2           6
 #define LED_DATA_RIBBON_3           0
 #define LED_DATA_RIBBON_4           0
 #define LED_DATA_RIBBON_5           0
 
-#define ULTRASONIC_SENSOR_1_TRIG    7
-#define ULTRASONIC_SENSOR_1_ECHO    6
-#define ULTRASONIC_SENSOR_2_TRIG    0
-#define ULTRASONIC_SENSOR_2_ECHO    0
-#define ULTRASONIC_SENSOR_3_TRIG    0
-#define ULTRASONIC_SENSOR_3_ECHO    0
-
-#define HAND_FLASH_DELAY_DEFAULT    5000
-#define HAND_FLASH_DURATION         1000
-#define HAND_BRIGHTNESS             100 // Peak brightness of hand
+#define ULTRASONIC_SENSOR_1_TRIG    22
+#define ULTRASONIC_SENSOR_1_ECHO    23
+#define ULTRASONIC_SENSOR_2_TRIG    24
+#define ULTRASONIC_SENSOR_2_ECHO    25
+#define ULTRASONIC_SENSOR_3_TRIG    26
+#define ULTRASONIC_SENSOR_3_ECHO    27
 
 //Misc LED initialisation stuff
 #define COLOUR_ORDER       GRB
-#define LED_TYPE           WS2812B
+#define LED_TYPE           WS2811
 #define BRIGHTNESS         64
 #define NUM_LEDS_HAND      120 //120 LEDs on the strip we'll be using
 #define NUM_LEDS_RIBBON    150      
 #define UPDATES_PER_SECOND 100
 #define PULSE_LENGTH       30
-#define PULSE_DELAY        25
+#define PULSE_DELAY        10
 #define NUM_RIBBONS        3
-
 CRGB leds_hand[NUM_LEDS_HAND];
 CRGB leds_ribbons[NUM_RIBBONS][NUM_LEDS_RIBBON];
 CRGBPalette16              currentPalette; //sets what colours we can use
@@ -54,7 +49,7 @@ TBlendType                 currentBlending;
 //Misc Ultrasonic sensor stuff
 #define NUM_ULTRA_SENSORS   3
 #define DISTANCE_THRESHOLD  400 //change this as per your measurements :))
-#define DIST_TO_DELAY       10 //the number distance is multiplied to to get the delay in microseconds
+#define DIST_TO_DELAY       80 //the number distance is multiplied to to get the delay in microseconds
 
 struct Sensor {
   int trig;
@@ -65,12 +60,11 @@ struct Sensor sensors[NUM_ULTRA_SENSORS];
 
 // Global Variables
 int brightness = 0;
-int ribbon_i = 0;
+int ribbon_i = 1 - PULSE_LENGTH;
 int hand_i = 0;
 uint32_t ribbon_time = 0;
-uint32_t hand_time = 0;
-uint32_t last_hand_time = 0; 
-uint32_t start_flash = 0; // If 0, that means not flashing
+int hand_time = 0;
+
 /*
 //This function returns an array containing how far away an object is from each sensor
 int * distanceDetected(){
@@ -85,12 +79,12 @@ int * distanceDetected(){
 
 //This function returns the closest distance detected by the ultrasonic sensors
 int getClosest() {
-  int closest = getDistance(sensors[0].trig, sensors[0].echo);
-/* TODO : commented out for testing
-  for (int i = 1; i < NUM_ULTRA_SENSORS; i++) {
+  int closest = 401;
+
+  for (int i = 0; i < NUM_ULTRA_SENSORS; i++) {
     int dist = getDistance(sensors[i].trig, sensors[i].echo);
     if (dist < closest) closest = dist;
-  }*/
+  }
   
   return closest;
 }
@@ -156,11 +150,12 @@ void setup() {
   //Initialise LEDs
   FastLED.addLeds<LED_TYPE, LED_DATA_HAND, COLOUR_ORDER>(leds_hand, NUM_LEDS_HAND).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_0, COLOUR_ORDER>(leds_ribbons[0], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
-  if (NUM_RIBBONS > 1) FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_1, COLOUR_ORDER>(leds_ribbons[1], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
-  if (NUM_RIBBONS > 2) FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_2, COLOUR_ORDER>(leds_ribbons[2], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
-  if (NUM_RIBBONS > 3) FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_3, COLOUR_ORDER>(leds_ribbons[3], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
-  if (NUM_RIBBONS > 4) FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_4, COLOUR_ORDER>(leds_ribbons[4], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
-
+  FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_1, COLOUR_ORDER>(leds_ribbons[0], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_2, COLOUR_ORDER>(leds_ribbons[0], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
+/*
+  FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_3, COLOUR_ORDER>(leds_ribbons[3], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_DATA_RIBBON_4, COLOUR_ORDER>(leds_ribbons[4], NUM_LEDS_RIBBON).setCorrection( TypicalLEDStrip );
+*/
 
 
   FastLED.setBrightness(  BRIGHTNESS );
@@ -176,8 +171,6 @@ void loop() {
   //hand_i++;
 
   ribbon();
-  //hand();
-  //hand_flash();
   //Check sensors
   /*
   int * buf = distanceDetected();
@@ -216,7 +209,6 @@ void loop() {
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex, int updated_brightness)
 {
-    
     for( int i = 0; i < NUM_LEDS_HAND; i++) {
         leds_hand[i] = ColorFromPalette( currentPalette, colorIndex, updated_brightness, currentBlending);
         colorIndex += 3;
@@ -229,110 +221,19 @@ void ribbon() {
   }
 
   ribbon_time = millis() + PULSE_DELAY;
-  
+
   for (int i = 0; i < NUM_RIBBONS; i++) {
-    
-    if (ribbon_i == 0) {
-      leds_ribbons[i][NUM_LEDS_HAND - 1] = CRGB(0,0,0);
-      //light_seg(ribbon_i, 15, CRGB(255,0,0), i); // turn on
-      
-    } else {
-      leds_ribbons[i][ribbon_i - 1] = CRGB(0, 0, 0); // turn off unused LED from previous sequence
-      leds_ribbons[i][(ribbon_i + PULSE_LENGTH - 1) /*% NUM_LEDS_RIBBON*/] = CRGB(255,0,0); // turn on newly required LED
-      
-      for (int i_trail = 1 - PULSE_LENGTH; i_trail < PULSE_LENGTH; i_trail++) {
-          if (i_trail >= 0) {
-            leds_ribbons[i][ribbon_i + i_trail] = CRGB((int)(255 * ((float)i_trail/(float)PULSE_LENGTH)), 0, 0);
+      for (int i_trail = 0; i_trail < PULSE_LENGTH; i_trail++) {
+          if (i_trail + ribbon_i >= 0) {
+            leds_ribbons[i][(ribbon_i + i_trail) % NUM_LEDS_RIBBON] = CRGB((int)(255 * ((float)i_trail/(float)PULSE_LENGTH)), 0, 0);
           }
       }
 
-    }
-
-
   }
-
   
   FastLED.show();
   ribbon_i++;
-  ribbon_i = ribbon_i % NUM_LEDS_HAND;
-}
-
-
-
-
-// Sets len LEDs starting at start to colour col
-// Wraps around
-void light_seg(int start, int len, CRGB col, int ribbon_num) {
-  if (ribbon_num != -1){
-    for (int i = 0; i < len; i++) {
-      leds_ribbons[ribbon_num][(start + i) % NUM_LEDS_HAND] = col;
-    }
-  } else {
-    for (int i = 0; i < len; i++) {
-      leds_hand[(start + i) % NUM_LEDS_HAND] = col;
-    }
+  if (ribbon_i >= NUM_LEDS_RIBBON) {
+    ribbon_i = 0; 
   }
-}
-
-void hand() {
-  if (start_flash != 0 && (millis() >= (last_hand_time))) {
-    start_flash = 0;
-    light_all_hand(CRGB(0,0,0));
-  } else if (start_flash) {
-    // Still flashing
-    return;
-  }
-
-  if (millis() >= hand_time) {
-    // flash
-    start_flash = millis();
-    last_hand_time = millis() + HAND_FLASH_DURATION;
-    // Set next hand_time
-    hand_time = last_hand_time + HAND_FLASH_DELAY_DEFAULT;
-    
-    return;
-  }
-
-  int closest = getClosest();
-  Serial.println(closest);
-  if (closest > DISTANCE_THRESHOLD) {
-    return;
-  } else {
-    int curr_delay = hand_time - last_hand_time;
-    if (curr_delay > (closest * DIST_TO_DELAY)) {
-      hand_time = last_hand_time + (closest * DIST_TO_DELAY);
-      Serial.println("delay");
-      Serial.println(closest * DIST_TO_DELAY);
-    }
-  }
-
-
-}
-
-void hand_flash() {
-  if (start_flash) {
-    //flashing
-    
-    int calc_brightness = 0;
-    if (millis() >= (start_flash + 4 * HAND_FLASH_DURATION/5)) {
-      // brightness down
-      calc_brightness = (HAND_BRIGHTNESS * (1 - (float)(millis() - start_flash - 4 * HAND_FLASH_DURATION/5)/(HAND_FLASH_DURATION/5)));
-    } else if (millis() <= (start_flash + HAND_FLASH_DURATION/5)) {
-      // brightness up
-      calc_brightness = (HAND_BRIGHTNESS * (float)(millis() - start_flash)/(HAND_FLASH_DURATION/5));
-    } else {
-      calc_brightness = HAND_BRIGHTNESS;
-    }
-    light_all_hand(CRGB(calc_brightness,(int)(calc_brightness/1.25),0));
-    
-
-  }
-}
-
-
-void light_all_hand(CRGB col) {
-  for (int i = 0; i < NUM_LEDS_HAND; i++) {
-    leds_hand[i] = col; 
-  }
-  FastLED.show();
 }
